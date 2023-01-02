88be58e8c1331e55cbfe56c7f1bc4e70b039ddba
Yossy Adirta <yossyadirta@gmail.com>
2023-01-02 11:53:49 +0700

    Added express and listener

--- build/index.js
+++ build/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("test ini dari hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.routes();
+    }
+    routes() {
+        this.app.route("/").get((req, res) => {
+            res.send("Ini adalah route menggunakan TS");
+        });
+    }
+}
+const port = 8000;
+const app = new App().app;
+app.listen(port, () => {
+    console.log("Port: " + port);
+});
+// const app = express();
+// app.route("/").get((req, res) => {
+//   res.send("Hello World");
+// });
+// app.listen(8000);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("test ini dari hello world");
+import express, { Application, Request, Response } from "express";
+
+class App {
+  public app: Application;
+  constructor() {
+    this.app = express();
+    this.routes();
+  }
+
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response) => {
+      res.send("Ini adalah route menggunakan TS");
+    });
+  }
+}
+
+const port: number = 8000;
+
+const app = new App().app;
+app.listen(port, () => {
+  console.log("Port: " + port);
+});
+
+// const app = express();
+
+// app.route("/").get((req, res) => {
+//   res.send("Hello World");
+// });
+
+// app.listen(8000);
