40bb366195d374467972b02ec9b82c2df2617209
Yossy Adirta <yossyadirta@gmail.com>
2023-01-08 18:10:20 +0700

    login and auth done

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import hashPassword from "../helpers/hashPassword";
+import Authentication from "../helpers/Authentication";
 
 const db = require("../db/models");
 
@@ -7,7 +7,7 @@ class AuthController {
   register = async (req: Request, res: Response): Promise<Response> => {
     const { username, password } = req.body;
 
-    const hashedPassword: string = await hashPassword.hash(password);
+    const hashedPassword: string = await Authentication.hashPassword(password);
 
     await db.User.create({
       username,
@@ -17,9 +17,37 @@ class AuthController {
     return res.send("sukses registrasi");
   };
 
-  login(req: Request, res: Response): Response {
-    return res.send("");
-  }
+  login = async (req: Request, res: Response): Promise<Response> => {
+    const { username, password } = req.body;
+
+    const user = await db.User.findOne({
+      where: { username },
+    });
+
+    //check password
+    if (user) {
+      let compare = await Authentication.comparePassword(
+        password,
+        user.password
+      );
+
+      if (compare) {
+        let access_token = Authentication.generateToken(
+          user.id,
+          username,
+          user.password
+        );
+        return res.send({
+          access_token,
+        });
+      }
+    }
+    return res.send("email/password salah");
+  };
+
+  profile = async (req: Request, res: Response): Promise<Response> => {
+    return res.send(req.app.locals.credential);
+  };
 }
 
 export default new AuthController();
--- server/src/middlewares/AuthMiddleware.ts
+++ server/src/middlewares/AuthMiddleware.ts
@@ -1,11 +1,24 @@
 import { Request, Response, NextFunction } from "express";
+import Authentication from "../helpers/Authentication";
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  console.log("ini adalah middleware");
-  // next();
-  let auth = false;
-  if (auth) {
-    next();
+  if (!req.headers.authorization) {
+    return res.status(401).send("no token");
   }
-  return res.send("un-authenticated");
+
+  const access_token: string = req.headers.authorization.split(" ")[1];
+
+  try {
+    const credential: string | object =
+      Authentication.verifyToken(access_token);
+
+    if (credential) {
+      req.app.locals.credential = credential;
+      next();
+    } else {
+      return res.send("Invalid token");
+    }
+  } catch (error) {
+    res.send(error);
+  }
 };
